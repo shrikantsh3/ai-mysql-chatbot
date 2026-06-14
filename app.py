651fb1ee2864062c596055af5e47e2cbdf58fcc4
Shrikant Sahu <shrikantsh3@gmail.com>
2026-06-14 20:24:33 +0530

    app.py file updated

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class ChatRequest(BaseModel):
 
 @app.get("/")
 def home():
-    return {"status": "AI API is running smoothly"}
+    return {"status": "AI API is running smoothly & prominent"}
 
 @app.post("/api/chat")
 async def chat(payload: ChatRequest):
